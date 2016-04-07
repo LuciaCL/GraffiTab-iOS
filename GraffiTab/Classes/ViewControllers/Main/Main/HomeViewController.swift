1bf0545c7b693206f4603e191ffae3c392b6ea40
Georgi Christov <georgi.christov89@gmail.com>
2016-04-07 11:42:13 +0100

    Initial version of the Home screen.

---
+++ GraffiTab/Classes/ViewControllers/Main/Main/HomeViewController.swift
@@ -0,0 +1,101 @@
+//
+//  HomeViewController.swift
+//  GraffiTab
+//
+//  Created by Georgi Christov on 06/04/2016.
+//  Copyright © 2016 GraffiTab. All rights reserved.
+//
+
+import UIKit
+import CarbonKit
+
+class HomeViewController: BackButtonViewController, CarbonTabSwipeNavigationDelegate {
+
+    @IBOutlet weak var createBtn: UIButton!
+    
+    var carbonTabSwipeNavigation: CarbonTabSwipeNavigation?
+    var controllers: [AnyObject]?
+    var titles: [AnyObject]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        setupCarbonKit()
+        setupButtons()
+        
+        configureTabBasedViews(0)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onClickCreate(sender: AnyObject) {
+        print("CREATE")
+    }
+    
+    // MARK: - Navigation
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+    }
+    
+    // MARK: - CarbonKitTabSwipeDelegate
+    
+    func carbonTabSwipeNavigation(carbonTabSwipeNavigation: CarbonTabSwipeNavigation, viewControllerAtIndex index: UInt) -> UIViewController {
+//        switch index {
+//        case 0:
+//            return self.storyboard!.instantiateViewControllerWithIdentifier("ViewControllerOne")! as! ViewControllerOne
+//        case 1:
+//            return self.storyboard!.instantiateViewControllerWithIdentifier("ViewControllerTwo")! as! ViewControllerTwo
+//        default:
+//            return self.storyboard!.instantiateViewControllerWithIdentifier("ViewControllerThree")! as! ViewControllerThree
+//        }
+        let vs = UIViewController()
+        return vs
+    }
+    
+    func carbonTabSwipeNavigation(carbonTabSwipeNavigation: CarbonTabSwipeNavigation, didMoveAtIndex index: UInt) {
+        configureTabBasedViews(Int(carbonTabSwipeNavigation.currentTabIndex))
+    }
+    
+    func configureTabBasedViews(index: Int) {   
+        self.title = titles![index] as? String
+    }
+    
+    // MARK: - Setup
+    
+    func setupCarbonKit() {
+        controllers = [UIImage(named: "home")!, UIImage(named: "ic_whatshot_white")!, UIImage(named: "ic_thumb_up_white")!, UIImage(named: "ic_access_time_white")!]
+        titles = ["Home", "Trending", "Recommended", "Recent"]
+        carbonTabSwipeNavigation = CarbonTabSwipeNavigation(items: controllers, delegate: self)
+        carbonTabSwipeNavigation!.insertIntoRootViewController(self)
+        
+        // Styling.
+        let tintColor = UIColor(hexString: Colors.Main)
+        carbonTabSwipeNavigation!.setIndicatorColor(tintColor)
+        carbonTabSwipeNavigation!.setTabExtraWidth(30)
+        
+        for (index, _) in (controllers?.enumerate())! {
+            carbonTabSwipeNavigation!.carbonSegmentedControl!.setWidth(self.view.frame.width / CGFloat((controllers?.count)!), forSegmentAtIndex: index)
+        }
+        
+        carbonTabSwipeNavigation!.setNormalColor(UIColor.blackColor().colorWithAlphaComponent(0.2))
+        carbonTabSwipeNavigation!.setSelectedColor(tintColor!, font: UIFont.boldSystemFontOfSize(14))
+    }
+    
+    func setupButtons() {
+        createBtn.layer.cornerRadius = createBtn.frame.size.width / 2
+        createBtn.layer.shadowRadius = 3.0
+        createBtn.layer.shadowColor = UIColor.blackColor().CGColor;
+        createBtn.layer.shadowOffset = CGSizeMake(1.6, 1.6)
+        createBtn.layer.shadowOpacity = 0.5
+        createBtn.layer.masksToBounds = false
+        createBtn.backgroundColor = UIColor(hexString: Colors.Main)
+        
+        self.view.bringSubviewToFront(createBtn)
+    }
+}
--- Podfile
+++ Podfile
@@ -3,24 +3,33 @@ platform :ios, '8.0'
 # Uncomment this line if you're using Swift
 use_frameworks!
 
+def common_pods
+    pod 'AlamofireObjectMapper'
+    pod 'AlamofireImage'
+    pod 'TPKeyboardAvoiding'
+    pod 'RNActivityView'
+    pod 'SlideMenuControllerSwift'
+    pod 'SwiftHEXColors'
+    pod 'UIActionSheet+Blocks'
+    pod 'SCLAlertView'
+    pod 'FBSDKLoginKit'
+    pod 'PTPPlaceholderTextView'
+    pod 'JTMaterialTransition'
+    pod 'RSKGrowingTextView'
+    pod 'MZFormSheetPresentationController'
+    pod 'iCarousel'
+    pod 'DZNEmptyDataSet'
+    pod 'SABlurImageView'
+    pod 'JVFloatLabeledTextField'
+    pod 'EAIntroView'
+    pod 'CSStickyHeaderFlowLayout'
+    pod 'CarbonKit'
+end
+
 target 'GraffiTab' do
-  pod 'AlamofireObjectMapper'
-  pod 'AlamofireImage'
-  pod 'TPKeyboardAvoiding'
-  pod 'RNActivityView'
-  pod 'SlideMenuControllerSwift'
-  pod 'SwiftHEXColors'
-  pod 'UIActionSheet+Blocks'
-  pod 'SCLAlertView'
-  pod 'FBSDKLoginKit'
-  pod 'PTPPlaceholderTextView'
-  pod 'JTMaterialTransition'
-  pod 'RSKGrowingTextView'
-  pod 'MZFormSheetPresentationController'
-  pod 'iCarousel'
-  pod 'DZNEmptyDataSet'
-  pod 'SABlurImageView'
-  pod 'JVFloatLabeledTextField'
-  pod 'EAIntroView'
-  pod 'CSStickyHeaderFlowLayout'
+    common_pods
+end
+
+target 'GraffiTab Release' do
+    common_pods
 end
