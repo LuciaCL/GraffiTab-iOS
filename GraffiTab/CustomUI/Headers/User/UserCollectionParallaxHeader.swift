537341a3c6af8bef2c8bd695260472e892ffff2a
Georgi Christov <georgi.christov89@gmail.com>
2016-04-16 14:03:26 +0100

    Design for user profile.

---
+++ GraffiTab/CustomUI/Headers/User/UserCollectionParallaxHeader.swift
@@ -0,0 +1,18 @@
+//
+//  UserCollectionParallaxHeader.swift
+//  GraffiTab
+//
+//  Created by Georgi Christov on 16/04/2016.
+//  Copyright © 2016 GraffiTab. All rights reserved.
+//
+
+import UIKit
+
+class UserCollectionParallaxHeader: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
