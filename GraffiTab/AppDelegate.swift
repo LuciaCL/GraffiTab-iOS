5569b85235e0a51813e593a76cb00419dc815bd9
Georgi Christov <georgi.christov89@gmail.com>
2016-09-07 22:03:28 +0100

    Released v18.

--- GraffiTab/AppDelegate.swift
+++ GraffiTab/AppDelegate.swift
@@ -322,8 +322,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         config.logEnabled = true
         config.httpsEnabled = true
         
-        config.domain = "localhost:8091"
-        config.httpsEnabled = false
+//        config.domain = "localhost:8091"
+//        config.httpsEnabled = false
         
         if !AppConfig.sharedInstance.isAppStore { // We are deploying to dev or testing locally.
             #if DEBUG // Show full debug traces.
